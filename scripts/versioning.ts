b248ef707b3eb5ea9668fab8eecb6a2d70eb51c2
Constantine Theocharis <cthe@mailbox.org>
2021-01-22 21:38:07 +0000

    Rewrite versioning.ts to use promises
    Fixes a potential (very unlikely) race condition.

--- scripts/versioning.ts
+++ scripts/versioning.ts
@@ -1,30 +1,66 @@
 const childProcess = require("child_process");
 const fs = require("fs");
 
-function writeToEnv(key: string = "", value: string = "") {
-    const empty = key === "" && value === "";
-
-    if (empty) {
-        fs.writeFile(".env.local", "", () => {});
-    } else {
-        fs.appendFile(".env.local", `${key}='${value.trim()}'\n`, (err: Error | null) => {
-            if (err) console.log(err);
-        });
-    }
+/**
+ * Clear .env.local.
+ */
+async function clearEnv() {
+    await new Promise((resolve, reject) =>
+        fs.writeFile(".env.local", "", (err: Error | null) => {
+            if (err) reject(err);
+            resolve();
+        })
+    );
 }
 
-// reset .env file
-writeToEnv();
+/**
+ * Add an environment variable entry to .env.local.
+ *
+ * @param key - the variable key.
+ * @param value - the variable value.
+ */
+async function writeToEnv(key: string, value: string) {
+    const entry = `${key}='${value.trim()}'\n`;
+    await new Promise((resolve, reject) =>
+        fs.appendFile(".env.local", entry, (err: Error | null) => {
+            if (err) reject(err);
+            resolve();
+        })
+    );
+}
 
+/**
+ * Execute a command, returning stdout.
+ *
+ * @param command - the command to execute.
+ * @returns the stdout of the command.
+ */
+async function exec(command: string): Promise<string> {
+    return await new Promise((resolve, reject) =>
+        childProcess.exec(command, (err: Error | null, stdout: string) => {
+            if (err) reject(err);
+            resolve(stdout);
+        })
+    );
+}
 
-childProcess.exec("git rev-parse --abbrev-ref HEAD", (err: Error | null, stdout: string) => {
-    writeToEnv("REACT_APP_VERSION_BRANCH", stdout);
-});
-childProcess.exec("git rev-parse HEAD", (err: Error| null, stdout: string) => {
-    writeToEnv("REACT_APP_DEV_VERSION", stdout);
-});
-childProcess.exec("grep 'shared' yarn.lock | grep -o -E -e '[0-9a-f]{40}'", (err: Error| null, stdout: string) => {
-    writeToEnv("REACT_APP_DEV_SHARED_VERSION", stdout);
-});
+/**
+ * Write environment variables to .env.local
+ */
+async function main() {
+    await clearEnv();
 
+    await exec("git rev-parse --abbrev-ref HEAD").then((stdout) =>
+        writeToEnv("REACT_APP_VERSION_BRANCH", stdout)
+    );
 
+    await exec("git rev-parse HEAD").then((stdout) =>
+        writeToEnv("REACT_APP_DEV_VERSION", stdout)
+    );
+
+    await exec("grep 'shared' yarn.lock | grep -oEe '[0-9a-f]{40}'").then((stdout) =>
+        writeToEnv("REACT_APP_DEV_SHARED_VERSION", stdout)
+    );
+}
+
+main().catch(console.error);
