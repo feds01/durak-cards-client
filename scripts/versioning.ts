b477a1740e105af140bab4e4910ccfd2897f2928
alex <alexander.fedotov.uk@gmail.com>
2021-01-22 21:28:39 +0000

    fix bug with flaky 'skip' button being disabled/enabled.

--- scripts/versioning.ts
+++ scripts/versioning.ts
@@ -1,7 +1,7 @@
 const childProcess = require("child_process");
 const fs = require("fs");
 
-function writeToEnv(key: string = "", value: string = "") {
+async function writeToEnv(key: string = "", value: string = "") {
     const empty = key === "" && value === "";
 
     if (empty) {
@@ -14,17 +14,17 @@ function writeToEnv(key: string = "", value: string = "") {
 }
 
 // reset .env file
-writeToEnv();
+(async () => {
+    await writeToEnv();
 
-
-childProcess.exec("git rev-parse --abbrev-ref HEAD", (err: Error | null, stdout: string) => {
-    writeToEnv("REACT_APP_VERSION_BRANCH", stdout);
-});
-childProcess.exec("git rev-parse HEAD", (err: Error| null, stdout: string) => {
-    writeToEnv("REACT_APP_DEV_VERSION", stdout);
-});
-childProcess.exec("grep 'shared' yarn.lock | grep -o -E -e '[0-9a-f]{40}'", (err: Error| null, stdout: string) => {
-    writeToEnv("REACT_APP_DEV_SHARED_VERSION", stdout);
-});
-
+    await childProcess.exec("git rev-parse --abbrev-ref HEAD", (err: Error | null, stdout: string) => {
+        writeToEnv("REACT_APP_VERSION_BRANCH", stdout);
+    });
+    await childProcess.exec("git rev-parse HEAD", (err: Error| null, stdout: string) => {
+        writeToEnv("REACT_APP_DEV_VERSION", stdout);
+    });
+    await childProcess.exec("grep 'shared' yarn.lock | grep -o -E -e '[0-9a-f]{40}'", (err: Error| null, stdout: string) => {
+        writeToEnv("REACT_APP_DEV_SHARED_VERSION", stdout);
+    });
+})()
 
